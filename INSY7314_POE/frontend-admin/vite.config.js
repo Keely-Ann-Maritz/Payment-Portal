175068e95bf621f0eb5aa8bef0c7977711ea79f9
Keely-Ann <ST10085428@vcconnect.edu.za>
2025-10-24 11:59:59 +0200

    Added new Frontend Admin folder and changed ports

---
+++ INSY7314_POE/frontend-admin/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // server: {
+  
+  //     https: {
+  //       key: fs.readFileSync('./certs/localhost+1-key.pem'),
+  //       cert: fs.readFileSync('./certs/localhost+1.pem'),
+  //     }
+  //   }
+})
